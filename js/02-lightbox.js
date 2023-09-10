75e130bf685da8756ae664930c4a75e7dd0bdd9d
Sergio <129672743+SergioNedybaliuk@users.noreply.github.com>
2023-09-10 13:26:54 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(" .gallery")
+const galleryList = galleryItems.map(({preview, original, description}) =>
+  `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img src="${preview}" alt="${description}" class="gallery__image"/>
+      </a>
+      </li>`)
+  .join("")
+
+  gallery.innerHTML = galleryList;
+
+  const lightBox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
